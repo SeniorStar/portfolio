d55e76cd8c43606533e58cb378d73345e7f6ac1c
SeniorStar <dev.star1202@gmail.com>
2024-11-14 13:55:08 +0200

    catalin-port

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,7 +3,7 @@ export const personalData = {
   profile: '/profile.png',
   designation: "Senior Full Stack Developer",
   description: "My name is Catalin. I am a professional and enthusiastic Web and Mobile Developer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-  email: 'catalistan81@outlook.com',
+  email: 'catalin0530@gmail.com',
   phone: '+40728583922',
   address: 'Orhei no.4, Piatra Neamt',
   github: 'https://github.com/AustinSLight',
